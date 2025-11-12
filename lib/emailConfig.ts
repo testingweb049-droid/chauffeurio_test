f2e93ef04963b011662e2399b2edaa1e82de7983
mussaddiqmahmood7 <mussaddiqmahmood7@gmail.com>
2025-11-12 19:50:30 +0500

    email pass and user dynamic

--- lib/emailConfig.ts
+++ lib/emailConfig.ts
@@ -4,16 +4,6 @@ export const emailConfig = {
   secure: true,
   auth: {
     user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASSWORD,
+    pass: process.env.EMAIL_PASS,
   },
 };
-
-// export const emailConfig = {
-//       host: "smtp.hostinger.com",
-//       port: 465,
-//       secure: true,
-//       auth: {
-//         user: "info@chauffeurio.com",
-//         pass:"Valenciaspain2025@@"
-//       },
-//     };
