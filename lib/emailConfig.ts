9366ec14ac6b6104a707f3f40d744fb5221ebe69
hammadsandhuu <hammadsandhuu@gmail.com>
2025-11-13 15:11:19 +0500

    Update email configuration with hardcoded SMTP settings

--- lib/emailConfig.ts
+++ lib/emailConfig.ts
@@ -1,19 +1,19 @@
-export const emailConfig = {
-  host: process.env.EMAIL_HOST ,
-   port: 465,
-  secure: true,
-  auth: {
-    user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASSWORD,
-  },
-};
-
 // export const emailConfig = {
-//       host: "smtp.hostinger.com",
-//       port: 465,
-//       secure: true,
-//       auth: {
-//         user: "info@chauffeurio.com",
-//         pass:"Valenciaspain2025@@"
-//       },
-//     };
+//   host: process.env.EMAIL_HOST ,
+//    port: 465,
+//   secure: true,
+//   auth: {
+//     user: process.env.EMAIL_USER,
+//     pass: process.env.EMAIL_PASSWORD,
+//   },
+// };
+
+export const emailConfig = {
+      host: "smtp.hostinger.com",
+      port: 465,
+      secure: true,
+      auth: {
+        user: "info@chauffeurio.com",
+        pass:"Valenciaspain2025@@"
+      },
+    };
