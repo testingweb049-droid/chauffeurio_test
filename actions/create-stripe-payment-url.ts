33236d37a24a2735c436cf676093abedee028b8d
moaz <143252699+bariq49@users.noreply.github.com>
2025-11-18 01:49:13 +0500

    mm

--- actions/create-stripe-payment-url.ts
+++ actions/create-stripe-payment-url.ts
@@ -3,7 +3,7 @@
 import Stripe from "stripe"
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY ?? "", {
-  apiVersion: "2025-10-29.clover",
+  apiVersion:"2025-09-30.clover",
 })
 
 export async function CreateStripePaymentURLAction(amount: number, orderId:string, secret:string) {
